c26e3925ec6bac5c7b75800c6949f1da06047b93
dream22301 <brilliantprincoekananda2009@gmail.com>
2026-02-03 14:00:34 +0700

    memperbaiki sidebar

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -10,13 +10,18 @@
 
 <body>
 <div class="min-h-screen flex">
-  <aside class="min-w-48 shadow-2xl shadow-black bg-gray-800 text-white">
+  <aside class="min-w-48 shadow-2xl shadow-black bg-gray-800 text-white flex items-center flex-col gap-2">
     <div class="icon">
       <img src="{{ asset('img/account.svg') }}" alt="img" class="w-20">
     </div>
+    <div class="bg-blend-difference bg-gray-500 w-38 border-black rounded-sm">
+      <label class="flex justify-center">User</label>
+    </div>
   </aside>
 
-  <main class="flex-1">Content</main>
+  <main class="flex justify-center items-center w-full">
+    <h1>dashboard text (for now)</h1>
+  </main>
 </div>
 
 </body>
