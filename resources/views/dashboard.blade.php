cb3d662db4ac9f23d349ef5181dd901fe19eeb6d
dream22301 <brilliantprincoekananda2009@gmail.com>
2026-02-09 10:05:52 +0700

    penggunaan layout agar lebih mudah :)

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,33 +1,7 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>Document</title>
-  @vite(['resources/css/app.css'])
-</head>
+@extends('layouts.dashboard-layout')
 
-<body>
-  <div class="grid grid-cols-[16rem_2fr] min-h-screen">
-    <aside class="bg-gray-500 grid grid-rows-[auto_1fr] h-full p-6 gap-12">
-      <div class="bg-amber-500 h-auto flex flex-rows items-center gap-8">
-        <img src="{{ asset('img/account.svg') }}" alt="account" class="icon">
-        <div class="">
-          <h3>User</h3>
-        </div>
-      </div>
-      <div class="bg-blue-600">
-        <div class="bg-gray-700 w-full h-16 ">
-          <p>test</p>
-        </div>
-      </div>
-      
-    </aside>
+@section('judul', 'homepage')
 
-    <main class="flex items-center justify-center">
-      <h1>text</h1>
-    </main>
-  </div>
-</body>
-</html>
+@section('content')
+<h1>test</h1>
+@endsection
--- resources/views/layouts/dashboard-layout.blade.php
+++ resources/views/layouts/dashboard-layout.blade.php
@@ -2,10 +2,38 @@
 <html lang="en">
 <head>
   <meta charset="UTF-8">
-  <title>@yield('title')</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>@yield('judul')</title>
   @vite(['resources/css/app.css'])
 </head>
+
 <body>
-  
+  <div class="grid grid-cols-[16rem_2fr] min-h-screen">
+    <aside class="bg-gray-500 grid grid-rows-[auto_1fr] h-full p-6 gap-4">
+      <div class="bg-amber-500 h-auto flex flex-rows items-center gap-8">
+        <img src="{{ asset('img/account.svg') }}" alt="account" class="icon">
+        <div class="">
+          <h3>User</h3>
+        </div>
+      </div>
+      <div class="flex flex-col gap-2 items-center">
+        <a href="#" class="bg-gray-700 w-50 h-8 rounded-md flex items-center justify-center">
+          Home
+        </a>
+        <a href="#" class="bg-gray-700 w-50 h-8 rounded-md flex items-center justify-center">
+          Dashboard
+        </a>
+        <a href="#" class="bg-gray-700 w-50 h-8 rounded-md flex items-center justify-center">
+          About
+        </a>
+      </div>
+      
+    </aside>
+
+    <main>
+      @yield('content')
+    </main>
+  </div>
 </body>
 </html>
